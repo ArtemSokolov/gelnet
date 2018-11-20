365f79aefa9716c82344f2c5dc48860cefe81838
Artem Sokolov <artem.sokolov@gmail.com>
2018-11-20 16:03:32 -0500

    Removed deprecated functions

--- R/gelnet.R
+++ R/gelnet.R
@@ -91,12 +91,13 @@ L1.ceiling <- function( X, y, a = rep(1,nrow(X)), d = rep(1,ncol(X)),
 #' @param d p-by-1 vector of feature weights
 #' @param P p-by-p feature association penalty matrix
 #' @param m p-by-1 vector of translation coefficients
-#' @param max.iter maximum number of iterations
+#' @param max_iter maximum number of iterations
 #' @param eps convergence precision
-#' @param w.init initial parameter estimate for the weights
-#' @param b.init initial parameter estimate for the bias term
-#' @param fix.bias set to TRUE to prevent the bias term from being updated (regression only) (default: FALSE)
-#' @param silent set to TRUE to suppress run-time output to stdout (default: FALSE)
+#' @param w_init initial parameter estimate for the weights
+#' @param b_init initial parameter estimate for the bias term
+#' @param fix_bias set to TRUE to prevent the bias term from being updated (regression only) (default: FALSE)
+#' @param silent set to TRUE to suppress run-time output to stdout; overrides verbose (default: FALSE)
+#' @param verbose set to TRUE to see extra output; is overridden by silent (default: FALSE)
 #' @param balanced boolean specifying whether the balanced model is being trained (binary classification only) (default: FALSE)
 #' @param nonneg set to TRUE to enforce non-negativity constraints on the weights (default: FALSE )
 #' @return A list with two elements:
@@ -105,9 +106,9 @@ L1.ceiling <- function( X, y, a = rep(1,nrow(X)), d = rep(1,ncol(X)),
 #'   \item{b}{scalar, bias term for the linear model (omitted for one-class models)}
 #' }
 #' @export
-gelnet <- function( X, y, l1, l2, nFeats=NULL, a=rep(1,n), d=rep(1,p), P=diag(p), m=rep(0,p),
-                   max.iter=100, eps=1e-5, w.init=rep(0,p), b.init=NULL,
-                   fix.bias=FALSE, silent=FALSE, balanced=FALSE, nonneg=FALSE )
+gelnet_train <- function( X, y, l1, l2, nFeats=NULL, a=NULL, d=NULL, P=NULL, m=NULL,
+                         max_iter=100, eps=1e-5, w_init=rep(0,p), b_init=NULL,
+                         fix_bias=FALSE, silent=FALSE, verbose=FALSE, balanced=FALSE, nonneg=FALSE )
   {
     n <- nrow(X)
     p <- ncol(X)
@@ -117,7 +118,7 @@ gelnet <- function( X, y, l1, l2, nFeats=NULL, a=rep(1,n), d=rep(1,p), P=diag(p)
     {
         if( !silent ) cat( "Training a one-class model\n" )
         f.gel <- function(L1)
-        {gelnet.oneclass( X, L1, l2, d, P, m, max.iter, eps, w.init, silent, nonneg )}
+        {gelnet_oclr_opt( X, L1, l2, max_iter, eps, silent, verbose, nonneg, w_init, d, P, m )}
     }
 
     ## Binary classification
@@ -128,7 +129,7 @@ gelnet <- function( X, y, l1, l2, nFeats=NULL, a=rep(1,n), d=rep(1,p), P=diag(p)
           if( nlevels(y) > 2 )
               stop( "Labels belong to a multiclass task\nConsider training a set of one-vs-one or one-vs-rest models" )
           if( !silent ) cat( "Training a logistic regression model\n" )
-          if( is.null(b.init) ) b.init <- 0
+          if( is.null(b_init) ) b_init <- 0
 
           ## Convert the labels to {0,1}
           y <- factor(y)
@@ -137,18 +138,18 @@ gelnet <- function( X, y, l1, l2, nFeats=NULL, a=rep(1,n), d=rep(1,p), P=diag(p)
           y <- as.integer( y == levels(y)[1] )
 
           f.gel <- function(L1)
-          {gelnet.logreg( X, y, L1, l2, d, P, m, max.iter, eps,
-                         w.init, b.init, silent, balanced, nonneg )}
+          {gelnet_blr_opt( X, y, L1, l2, max_iter, eps, silent, verbose,
+                          balanced, nonneg, w_init, b_init, d, P, m )}
       }
 
     ## Regression
     else if( is.numeric(y) )
       {
           if( !silent ) cat( "Training a linear regression model\n" )
-          if( is.null(b.init) ) b.init <- sum(a*y) / sum(a)
+          if( is.null(b_init) ) b_init <- sum(a*y) / sum(a)
           f.gel <- function(L1)
-          {gelnet.lin( X, y, L1, l2, a, d, P, m, max.iter, eps,
-                      w.init, b.init, fix.bias, silent, nonneg )}
+          { gelnet_lin_opt( X, y, L1, l2, max_iter, eps, fix_bias, silent, verbose,
+                           nonneg, w_init, b_init, a, d, P, m) }
       }
     else
       { stop( "Unknown label type\ny must be a numeric vector, a 2-level factor or NULL" ) }
@@ -198,7 +199,7 @@ gelnet <- function( X, y, l1, l2, nFeats=NULL, a=rep(1,n), d=rep(1,p), P=diag(p)
 #' }
 #' @seealso \code{\link{gelnet}}
 #' @export
-gelnet.cv <- function( X, y, nL1, nL2, nFolds=5, a=rep(1,n), d=rep(1,p), P=diag(p), m=rep(0,p),
+gelnet_cv <- function( X, y, nL1, nL2, nFolds=5, a=rep(1,n), d=rep(1,p), P=diag(p), m=rep(0,p),
                    max.iter=100, eps=1e-5, w.init=rep(0,p), b.init=0,
                    fix.bias=FALSE, silent=FALSE, balanced=FALSE )
   {
@@ -302,9 +303,9 @@ gelnet.cv <- function( X, y, nL1, nL2, nFolds=5, a=rep(1,n), d=rep(1,p), P=diag(
                 j.tr <- which( vfa != k )
 
                 ## Train and evaluate the model
-                mm <- gelnet( X[j.tr,], y[j.tr], l1, l2, NULL, a[j.tr], d, P, m,
-                             max.iter, eps, w.init, b.init,
-                             fix.bias, silent, balanced )
+                mm <- gelnet_train( X[j.tr,], y[j.tr], l1, l2, NULL, a[j.tr], d, P, m,
+                                   max.iter, eps, w.init, b.init,
+                                   fix.bias, silent, balanced )
                 res[k] <- f.eval( mm, X[j.te,], y[j.te] )
 
                 if( !silent )
@@ -326,216 +327,6 @@ gelnet.cv <- function( X, y, nL1, nL2, nFolds=5, a=rep(1,n), d=rep(1,p), P=diag(
     mm.best
   }
 
-#' GELnet for linear regression
-#'
-#' Constructs a GELnet model for linear regression using coordinate descent.
-#'
-#' The method operates through cyclical coordinate descent.
-#' The optimization is terminated after the desired tolerance is achieved, or after a maximum number of iterations.
-#'
-#' @param X n-by-p matrix of n samples in p dimensions
-#' @param z n-by-1 vector of response values
-#' @param l1 coefficient for the L1-norm penalty
-#' @param l2 coefficient for the L2-norm penalty
-#' @param a n-by-1 vector of sample weights
-#' @param d p-by-1 vector of feature weights
-#' @param P p-by-p feature association penalty matrix
-#' @param m p-by-1 vector of translation coefficients
-#' @param max.iter maximum number of iterations
-#' @param eps convergence precision
-#' @param w.init initial parameter estimate for the weights
-#' @param b.init initial parameter estimate for the bias term
-#' @param fix.bias set to TRUE to prevent the bias term from being updated (default: FALSE)
-#' @param silent set to TRUE to suppress run-time output to stdout (default: FALSE)
-#' @param nonneg set to TRUE to enforce non-negativity constraints on the weights (default: FALSE )
-#' @return A list with two elements:
-#' \describe{
-#'   \item{w}{p-by-1 vector of p model weights}
-#'   \item{b}{scalar, bias term for the linear model}
-#' }
-#' @noRd
-gelnet.lin <- function( X, z, l1, l2, a = rep(1,n), d = rep(1,p), P = diag(p),
-                       m=rep(0,p), max.iter = 100, eps = 1e-5, w.init = rep(0,p),
-                       b.init = sum(a*z)/sum(a), fix.bias=FALSE, silent=FALSE, nonneg=FALSE )
-  {
-    n <- nrow(X)
-    p <- ncol(X)
-
-    ## Verify argument dimensionality
-    stopifnot( length(z) == n )
-    stopifnot( length(a) == n )
-    stopifnot( length(d) == p )
-    stopifnot( all( dim(P) == c(p,p) ) )
-    stopifnot( length(m) == p )
-    stopifnot( length(w.init) == p )
-    stopifnot( length(b.init) == 1 )
-    stopifnot( length(l1) == 1 )
-    stopifnot( length(l2) == 1 )
-
-    ## Verify name matching (if applicable)
-    if( is.null(colnames(X)) == FALSE && is.null(colnames(P)) == FALSE )
-      {
-        stopifnot( is.null( rownames(P) ) == FALSE )
-        stopifnot( all( colnames(X) == rownames(P) ) )
-        stopifnot( all( colnames(X) == colnames(P) ) )
-      }
-
-    ## Set the initial parameter estimates
-    S <- X %*% w.init + b.init
-    Pw <- P %*% (w.init - m)
-
-    NULL
-  }
-
-#' GELnet for logistic regression
-#'
-#' Constructs a GELnet model for logistic regression using the Newton method.
-#'
-#' The method operates by constructing iteratively re-weighted least squares approximations
-#' of the log-likelihood loss function and then calling the linear regression routine
-#' to solve those approximations. The least squares approximations are obtained via the Taylor series
-#' expansion about the current parameter estimates.
-#'
-#' @param X n-by-p matrix of n samples in p dimensions
-#' @param y n-by-1 vector of binary response labels (must be in {0,1})
-#' @param l1 coefficient for the L1-norm penalty
-#' @param l2 coefficient for the L2-norm penalty
-#' @param d p-by-1 vector of feature weights
-#' @param P p-by-p feature association penalty matrix
-#' @param m p-by-1 vector of translation coefficients
-#' @param max.iter maximum number of iterations
-#' @param eps convergence precision
-#' @param w.init initial parameter estimate for the weights
-#' @param b.init initial parameter estimate for the bias term
-#' @param silent set to TRUE to suppress run-time output to stdout (default: FALSE)
-#' @param balanced boolean specifying whether the balanced model is being trained
-#' @param nonneg set to TRUE to enforce non-negativity constraints on the weights (default: FALSE )
-#' @return A list with two elements:
-#' \describe{
-#'   \item{w}{p-by-1 vector of p model weights}
-#'   \item{b}{scalar, bias term for the linear model}
-#' }
-#' @noRd
-gelnet.logreg <- function( X, y, l1, l2, d = rep(1,p), P = diag(p), m = rep(0,p),
-                          max.iter = 100, eps = 1e-5, w.init = rep(0,p),
-                          b.init = 0, silent = FALSE, balanced = FALSE, nonneg = FALSE )
-  {
-    n <- nrow(X)
-    p <- ncol(X)
-
-    ## Verify argument dimensionality
-    stopifnot( sort(unique(y)) == c(0,1) )
-    stopifnot( length(y) == n )
-    stopifnot( length(d) == p )
-    stopifnot( all( dim(P) == c(p,p) ) )
-    stopifnot( length( w.init ) == p )
-    stopifnot( length( b.init ) == 1 )
-    stopifnot( length(l1) == 1 )
-    stopifnot( length(l2) == 1 )
-
-    ## Verify name matching (if applicable)
-    if( is.null(colnames(X)) == FALSE && is.null(colnames(P)) == FALSE )
-      {
-        stopifnot( is.null( rownames(P) ) == FALSE )
-        stopifnot( all( colnames(X) == rownames(P) ) )
-        stopifnot( all( colnames(X) == colnames(P) ) )
-      }
-
-    ## Set the initial parameter estimates
-    S <- X %*% w.init + b.init
-    Pw <- P %*% (w.init - m)
-
-    NULL
-  }
-
-#' GELnet for one-class regression
-#'
-#' Constructs a GELnet model for one-class regression using the Newton method.
-#'
-#' The function optimizes the following objective:
-#' \deqn{ -\frac{1}{n} \sum_i s_i - \log( 1 + \exp(s_i) ) + R(w) }
-#'  where
-#' \deqn{ s_i = w^T x_i }
-#' \deqn{ R(w) = \lambda_1 \sum_j d_j |w_j| + \frac{\lambda_2}{2} (w-m)^T P (w-m) }
-#' The method operates by constructing iteratively re-weighted least squares approximations
-#' of the log-likelihood loss function and then calling the linear regression routine
-#' to solve those approximations. The least squares approximations are obtained via the Taylor series
-#' expansion about the current parameter estimates.
-#'
-#' @param X n-by-p matrix of n samples in p dimensions
-#' @param l1 coefficient for the L1-norm penalty
-#' @param l2 coefficient for the L2-norm penalty
-#' @param d p-by-1 vector of feature weights
-#' @param P p-by-p feature association penalty matrix
-#' @param m p-by-1 vector of translation coefficients
-#' @param max.iter maximum number of iterations
-#' @param eps convergence precision
-#' @param w.init initial parameter estimate for the weights
-#' @param silent set to TRUE to suppress run-time output to stdout (default: FALSE)
-#' @param nonneg set to TRUE to enforce non-negativity constraints on the weights (default: FALSE )
-#' @return A list with one element:
-#' \describe{
-#'   \item{w}{p-by-1 vector of p model weights}
-#' }
-#' @noRd
-gelnet.oneclass <- function( X, l1, l2, d = rep(1,p), P = diag(p), m = rep(0,p),
-                            max.iter = 100, eps = 1e-5,
-                            w.init = rep(0,p), silent= FALSE, nonneg=FALSE )
-  {
-    n <- nrow(X)
-    p <- ncol(X)
-
-    ## Verify argument dimensionality
-    stopifnot( length(d) == p )
-    stopifnot( all( dim(P) == c(p,p) ) )
-    stopifnot( length(m) == p )
-    stopifnot( length(w.init) == p )
-    stopifnot( length(l1) == 1 )
-    stopifnot( length(l2) == 1 )
-
-    ## Verify name matching (if applicable)
-    if( is.null(colnames(X)) == FALSE && is.null(colnames(P)) == FALSE )
-      {
-        stopifnot( is.null( rownames(P) ) == FALSE )
-        stopifnot( all( colnames(X) == rownames(P) ) )
-        stopifnot( all( colnames(X) == colnames(P) ) )
-      }
-
-    ## Set the initial parameter estimates
-    w <- w.init
-
-    ## Run Newton's method
-    fprev <- gelnet_oclr_obj( w, X, l1, l2, d, P, m )
-    for( iter in 1:max.iter )
-      {
-        if( silent == FALSE )
-          {
-            cat( "Iteration", iter, ": " )
-            cat( "f =", fprev, "\n" )
-          }
-
-        ## Compute the current fit
-        s <- X %*% w
-        pr <- 1 / ( 1 + exp(-s) )
-
-        ## Compute the sample weights and active response
-        a <- pr * (1-pr)
-        z <- s + 1/pr
-
-        ## Run coordinate descent for the resulting regression problem
-        mm <- gelnet_lin_opt( X, z, l1, l2, a=a, d=d, P=P, m=m, max_iter = iter*2,
-                             eps=eps, w_init = w, b_init = 0, fix_bias = TRUE,
-                             silent = TRUE, nonneg=nonneg )
-        w <- mm$w
-
-        f <- gelnet_oclr_obj( w, X, l1, l2, d, P, m )
-        if( abs(f - fprev) / abs(fprev) < eps ) break
-        else fprev <- f
-      }
-
-    list( w = w )
-  }
-
 #' A GELnet model with a requested number of non-zero weights
 #'
 #' Binary search to find an L1 penalty parameter value that yields the desired
@@ -552,7 +343,7 @@ gelnet.oneclass <- function( X, l1, l2, d = rep(1,p), P = diag(p), m = rep(0,p),
 #'    and returns a typical GELnets model (list with w and b as its entries)
 #' @param nF the desired number of non-zero features
 #' @param l1s the right side of the search interval: search will start in [0, l1s]
-#' @param max.iter the maximum number of iterations of the binary search
+#' @param max_iter the maximum number of iterations of the binary search
 #'
 #' @return The model with the desired number of non-zero weights and the corresponding value of the
 #' L1-norm parameter. Returned as a list with three elements:
@@ -563,14 +354,14 @@ gelnet.oneclass <- function( X, l1, l2, d = rep(1,p), P = diag(p), m = rep(0,p),
 #' }
 #' @seealso \code{\link{L1.ceiling}}
 #' @noRd
-gelnet.L1bin <- function( f.gelnet, nF, l1s, max.iter=10 )
+gelnet.L1bin <- function( f.gelnet, nF, l1s, max_iter=10 )
   {
     ## Set up the search region
     L1top <- l1s
     L1bot <- 0
 
     ## Perform binary search
-    for( i in 1:max.iter )
+    for( i in 1:max_iter )
       {
         cat( "Binary search iteration", i, "\n" )
         l1 <- (L1top + L1bot) / 2
