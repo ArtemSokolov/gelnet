1b311ffcd55f116c0fdcfc6c79632e442af4057b
Artem Sokolov <artem.sokolov@gmail.com>
2018-11-26 22:47:34 -0500

    Split model and task definitions

--- R/def.R
+++ R/def.R
@@ -2,6 +2,31 @@
 ##
 ## by Artem Sokolov
 
+#' Composition operator for GELnet model definition
+#'
+#' @param lhs left-hand side of composition (current chain)
+#' @param rhs right-hand side of composition (new module)
+#' @export
+`+.geldef` <- function( lhs, rhs )
+{
+    if( class(lhs) == "geldef" ) gelnetComposite( rhs, lhs )
+    else gelnetComposite( lhs, rhs )
+}
+
+## S3 generic for model composition
+## Not exported
+gelnetComposite <- function( term, ... )
+{ UseMethod( "gelnetComposite" ) }
+
+## Default behavior for term composition
+## Not exported
+gelnetComposite.default <- function( term, mdl )
+{
+    for( i in names(term) )
+        mdl[[i]] <- term[[i]]
+    mdl
+}
+
 #' GELnet model definition for linear regression, binary classification or one-class problems.
 #'
 #' Infers the problem type and constructs the appropriate GELnet model definition.
@@ -27,58 +52,44 @@
 #' @param balanced boolean specifying whether the balanced model is being trained (binary classification only) (default: FALSE)
 #' @return A GELnet model definition
 #' @export
-gelnet <- function( X, y=NULL, a=NULL, nonneg=FALSE, balanced=FALSE )
+
+gelnet <- function( X )
 {
-    ## Check argument dimensionality
-    if( nrow(X) != length(y) )
+    if( !is.matrix(X) )
+        stop( "Argument must be a matrix" )
+    structure( list(X=X, l1=0, l2=0), class = "geldef" )
+}
+
+model_oclr <- function( nonneg=FALSE )
+{ structure( list(nonneg=nonneg), class = "taskdef" ) }
+
+model_blr <- function( y, nonneg=FALSE, balanced=FALSE )
+{
+    ## Hamdle non-factor character vectors
+    y <- factor(y)
+
+    if( nlevels(y) == 1 )
+        stop( "All labels are identical\n  Consider training a one-class model instead" )
+    if( nlevels(y) > 2 )
+        stop( paste0("Labels belong to a multiclass task\n  ",
+                     "Consider training a set of one-vs-one or one-vs-rest models") )
+
+    structure( list(y=y, nonneg=nonneg, balanced=balanced), class = "taskdef_sv" )
+}
+
+gelnetComposite.taskdef_sv <- function( term, mdl )
+{
+    if( nrow(mdl$X) != length(term$y) )
         stop( "Number of labels does not match the number of samples" )
+    UseMethod( "gelnetComposite", NULL )
+}
 
-    ## Create a bare-bones model
-    mdl <- list( X=X, nonneg=nonneg, l1=0, l2=0 )
-    class( mdl ) <- "geldef"
-
-    ## One-class
-    if( is.null(y) )
-    {
-        if( balanced ) warning( "Ignoring balanced setting in one-class models" )
-        if( !is.null(a) ) warning( "Ignoring sample weights in one-class models" )
-        mdl$type <- "one-class"
-    }
-
-    ## Binary classification
-    else if( is.factor(y) || is.character(y) )
-    {
-        if( !is.null(a) )
-            warning( "Ignoring sample weights in binary classification models" )
-
-        y <- factor(y)		## Handles non-factor character vectors
-
-        if( nlevels(y) == 1 )
-            stop( "All labels are identical\n  Consider training a one-class model instead" )
-        if( nlevels(y) > 2 )
-            stop( paste0("Labels belong to a multiclass task\n  ",
-                         "Consider training a set of one-vs-one or one-vs-rest models") )
-
-        ## Convert the labels to {0,1}
-        mdl$y <- as.integer( y == levels(y)[1] )
-        mdl$balanced <- balanced
-        mdl$type <- "binary"
-    }
-
-    ## Linear regression
-    else if( is.numeric(y) )
-    {
-        if( balanced ) warning( "Ignoring balanced setting in linear regression models" )
-        mdl$y <- y
-        if( !is.null(a) ) mdl$a <- a
-        mdl$type <- "regression"
-    }
-
-    ## Other
-    else
-    { stop( "Unknown label type\ny must be a numeric vector, a 2-level factor or NULL" ) }
-
-    mdl
+model_lin <- function( y, a=NULL, nonneg=FALSE )
+{
+    if( !is.numeric(y) ) stop( "Labels must be numeric" )
+    if( !is.null(a) && length(a) != length(y) )
+        stop( "The number of sample weights must match the number of labels" )
+    structure( list(y=y, a=a, nonneg=nonneg), class = "taskdef_sv" )
 }
 
 #' L1 regularizer for GELnet models
@@ -91,6 +102,7 @@ gelnet <- function( X, y=NULL, a=NULL, nonneg=FALSE, balanced=FALSE )
 #' @param l1 coefficient for the L1-norm penalty
 #' @param d p-by-1 vector of feature weights
 #' @return A regularizer definition that can be combined with a model definition using + operator
+#' @export
 gel_L1 <- function( l1, d=NULL )
 {
     if( l1 < 0 ) stop("L1 coefficient must be non-negative")
@@ -109,6 +121,8 @@ gel_L1 <- function( l1, d=NULL )
 #' @param l2 coefficient for the L2-norm penalty
 #' @param P p-by-p feature association penalty matrix
 #' @param m p-by-1 vector of translation coefficients
+#' @return A regularizer definition that can be combined with a model definition using + operator
+#' @export
 gel_L2 <- function( l2, P=NULL, m=NULL )
 {
     if( l2 < 0 ) stop("L2 coefficient must be non-negative")
@@ -117,20 +131,17 @@ gel_L2 <- function( l2, P=NULL, m=NULL )
     rglz
 }
 
-## Composition + operator for GELnet model definition
-`+.geldef` <- function( lhs, rhs )
-    { gelnetComposite( rhs, lhs ) }
-
-## S3 generic for model composition
-## Not exported
-gelnetComposite <- function( term, ... )
-    { UseMethod( "gelnetComposite" ) }
-
-## Default behavior for term composition
-## Not exported
-gelnetComposite.default <- function( term, mdl )
+#' Initializer for GELnet models
+#'
+#' Defines initial values for model weights and the bias term
+#'
+#' @param w_init p-by-1 vector of initial weight values
+#' @param b_init scalar, initial value for the bias term
+#' @export
+gel_init <- function( w_init, b_init )
 {
-    for( i in names(term) )
-        mdl[[i]] <- term[[i]]
-    mdl
+    initr <- list( w_init=w_init, b_init=b_init )
+    class(initr) <- "gelinit"
+    initr
 }
+
