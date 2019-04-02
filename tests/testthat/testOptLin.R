8786776ec1994e871c3ce2e1020ede1299bf5a6e
Artem Sokolov <artem.sokolov@gmail.com>
2019-04-02 12:13:49 -0400

    Test for L1 ceiling of linear regression

--- tests/testthat/testOptLin.R
+++ tests/testthat/testOptLin.R
@@ -21,6 +21,21 @@ gen_params_lin <- function()
     params
 }
 
+## Generates model definitions based on the provided set of parameters
+gen_modeldef_lin <- function( params )
+{
+    dd <- list()
+    dd[[1]] <- gelnet( params[[1]]$X ) + model_lin( params[[1]]$z ) +
+        rglz_L1( params[[1]]$l1 ) + rglz_L2( params[[1]]$l2 )
+    dd[[2]] <- dd[[1]] + model_lin( params[[2]]$z, params[[2]]$a ) +
+        rglz_L1( params[[2]]$l1, params[[2]]$d )
+    dd[[3]] <- dd[[2]] + rglz_L2( params[[3]]$l2, params[[3]]$P )
+    dd[[4]] <- dd[[3]] + rglz_L2( params[[4]]$l2, params[[4]]$P, params[[4]]$m )
+    dd[[5]] <- dd[[4]] + model_lin( params[[4]]$z, params[[4]]$a, fix_bias=TRUE )
+    dd[[6]] <- dd[[4]] + model_lin( params[[4]]$z, params[[4]]$a, nonneg=TRUE )
+    dd
+}
+
 test_that( "Linear regression training", {
     ## Silently trains a linear GELnet model using the provided parameters
     ftrain <- function( prms )
@@ -45,28 +60,30 @@ test_that( "Linear regression training", {
     expect_relopt( mm, ff )
 
     ## Test bias fixture
-    mm[[5]] <- do.call( gelnet_lin_opt, c(params[[4]], list(silent=TRUE, fix_bias=TRUE)) )
+    mm[[5]] <- ftrain( c(params[[4]], list(fix_bias=TRUE)) )
     expect_equal( mm[[5]]$b, with(params[[4]], sum(a*z)/sum(a)) )
     expect_lt( ff[[4]](mm[[4]]), ff[[4]](mm[[5]]) )
 
     ## Test non-negativity
-    mm[[6]] <- do.call( gelnet_lin_opt, c(params[[4]], list(silent=TRUE, nonneg=TRUE)) )
+    mm[[6]] <- ftrain( c(params[[4]], list(nonneg=TRUE)) )
     purrr::map( mm[[6]]$w, expect_gte, 0 )
     expect_lt( ff[[4]](mm[[4]]), ff[[4]](mm[[6]]) )
 
     ## Compose model definitions using the "grammar of modeling"
-    dd <- list()
-    dd[[1]] <- gelnet( params[[1]]$X ) + model_lin( params[[1]]$z ) +
-        rglz_L1( params[[1]]$l1 ) + rglz_L2( params[[1]]$l2 )
-    dd[[2]] <- dd[[1]] + model_lin( params[[2]]$z, params[[2]]$a ) +
-        rglz_L1( params[[2]]$l1, params[[2]]$d )
-    dd[[3]] <- dd[[2]] + rglz_L2( params[[3]]$l2, params[[3]]$P )
-    dd[[4]] <- dd[[3]] + rglz_L2( params[[4]]$l2, params[[4]]$P, params[[4]]$m )
-    dd[[5]] <- dd[[4]] + model_lin( params[[4]]$z, params[[4]]$a, fix_bias=TRUE )
-    dd[[6]] <- dd[[4]] + model_lin( params[[4]]$z, params[[4]]$a, nonneg=TRUE )
+    dd <- gen_modeldef_lin( params )
 
     ## Train based on model definitions
+    ## Ensure equivalence to direct calling of gelnet_lin_opt()
     mdls <- purrr::map( dd, gelnet_train, silent=TRUE )
     purrr::map2( mm, mdls, expect_equal )
+
+    ## Test the L1 ceiling computation
+    l1c <- with( params[[4]], l1c_lin( X, z, l2, a, d, P, m ) )
+    p1 <- purrr::list_modify( params[[4]], l1=l1c )
+    p2 <- purrr::list_modify( params[[4]], l1=l1c-0.01 )
+    m1 <- ftrain(p1)
+    m2 <- ftrain(p2)
+    expect_equal( sum(m1$w), 0 )
+    expect_length( which(m2$w != 0), 1 )
 })
 
