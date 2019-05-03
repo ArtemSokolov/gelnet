ef57d0cd686724437c6fdaad5df83ca8c2df3357
Artem Sokolov <artem.sokolov@gmail.com>
2019-05-03 16:47:22 -0400

    Update L1_ceiling

--- R/L1bin.R
+++ R/L1bin.R
@@ -5,58 +5,42 @@
 #' The largest meaningful value of the L1 parameter
 #'
 #' Computes the smallest value of the LASSO coefficient L1 that leads to an
-#'  all-zero weight vector for a given linear regression problem.
+#'  all-zero weight vector for a given gelnet model
 #'
 #' The cyclic coordinate descent updates the model weight \eqn{w_k} using a soft threshold operator
 #' \eqn{ S( \cdot, \lambda_1 d_k ) } that clips the value of the weight to zero, whenever the absolute
 #' value of the first argument falls below \eqn{\lambda_1 d_k}. From here, it is straightforward to compute
 #' the smallest value of \eqn{\lambda_1}, such that all weights are clipped to zero.
 #'
-#' @param X n-by-p matrix of n samples in p dimensions
-#' @param y n-by-1 vector of response values. Must be numeric vector for regression, factor with 2 levels for binary classification, or NULL for a one-class task.
-#' @param a n-by-1 vector of sample weights (regression only)
-#' @param d p-by-1 vector of feature weights
-#' @param P p-by-p feature association penalty matrix
-#' @param m p-by-1 vector of translation coefficients
-#' @param l2 coefficient for the L2-norm penalty
-#' @param balanced boolean specifying whether the balanced model is being trained (binary classification only) (default: FALSE)
+#' @param modeldef model definition constructed through gelnet() arithmetic
 #' @return The largest meaningful value of the L1 parameter (i.e., the smallest value that yields a model with all zero weights)
 #' @export
-L1.ceiling <- function( X, y, a = rep(1,nrow(X)), d = rep(1,ncol(X)),
-                       P = diag(ncol(X)), m = rep(0,ncol(X)), l2 = 1, balanced = FALSE )
-  {
-    if( is.null(y) )
-      {
+L1_ceiling <- function( modeldef )
+{
+
+    ## Direct the call to the appropriate C++ function
+    if( is.null(modeldef$y) )		
+    {
         ## One-class model
-        xy <- (apply( X/2, 2, mean ) + l2 * P %*% m) / d
-        return( max( abs(xy) ) )
-      }
+        l1c_oclr( modeldef$X, modeldef$l2,
+                 modeldef$d, modeldef$P, modeldef$m )
+    }
     else if( is.factor(y) )
-      {
+    {
         ## Binary model
         ## Convert the labels to {0,1}
-        y <- as.integer( y == levels(y)[1] )
-
-        a <- rep( 0.25, nrow(X) )
-        if( balanced )
-          {
-            jpos <- which( y==1 ); jneg <- which( y==0 )
-            a[jpos] <- a[jpos] * length(y) / ( length(jpos)*2 )
-            a[jneg] <- a[jneg] * length(y) / ( length(jneg)*2 )
-          }
-        z <- (y - 0.5) / 0.25
-        return( L1.ceiling( X, z, a, d, P, m, l2 ) )
-      }
+        y <- with( modeldef, as.integer(y == levels(y)[1]) )
+        l1c_blr( modeldef$X, y, modeldef$l2, modeldef$balanced,
+                modeldef$d, modeldef$P, modeldef$m )
+    }
     else if( is.numeric(y) )
-      {
+    {
         ## Liner regression model
-        stopifnot( nrow(X) == length(y) )
-        b1 <- sum( a*y ) / sum(a)
-        xy <- (apply( a*X*(y - b1), 2, mean ) + l2 * P %*% m) / d
-        return( max( abs(xy) ) )
-      }
+        l1c_lin( modeldef$X, modeldef$y, modeldef$l2, modeldef$a,
+                modeldef$d, modeldef$P, modeldef$m )
+    }
     else
-      { stop( "Unknown label type\ny must be a numeric vector, a 2-level factor or NULL" ) }
+    { stop( "Unknown label type\ny must be a numeric vector, a 2-level factor or NULL" ) }
   }
 
 #' A GELnet model with a requested number of non-zero weights
