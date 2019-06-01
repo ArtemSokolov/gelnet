830f0ccc500f301bc9f8c44ff520faaff24e5771
ArtemSokolov <artem.sokolov@gmail.com>
2019-05-31 22:22:57 -0400

    Linear regression testing now uses pre-generated data

--- tests/testthat/testOptLin.R
+++ tests/testthat/testOptLin.R
@@ -2,52 +2,39 @@ context( "Linear regression model training" )
 
 source( "custom.R" )
 
-## Generates parameters for linear regression testing
-gen_params_lin <- function()
+## Returns increasingly complex instantiations of f based on pre-generated
+##   linear regression data
+partial_lin <- function(f)
 {
-    ## Preset dimensionality
-    n <- 20
-    p <- 50
-
-    ## Preset L1 and L2 penalty coefficients and generate a sequence of
-    ##   models increasing in complexity
-    set.seed(100)
-    A <- matrix( rnorm(p*p), p, p )
-    params <- list( list(l1 = 0.1, l2 = 10, z = rnorm(n),
-                         X = matrix(rnorm(n*p), n, p)) )
-    params[[2]] <- purrr::list_modify( params[[1]], a = runif(n), d = runif(p) )
-    params[[3]] <- purrr::list_modify( params[[2]], P = t(A)%*%A/p )
-    params[[4]] <- purrr::list_modify( params[[3]], m = rnorm(p, sd=0.1) )
-    params
+    load( "data/lin.RData" )
+    vv <- purrr::accumulate( list(c("l1", "l2", "z", "X"),
+                                  c("a", "d"), "P", "m"), c )
+    pp <- purrr::map( vv, ~c(list(f), params[.x]) )
+    purrr::map( pp, ~do.call(purrr::partial, .x) )
 }
 
 ## Generates model definitions based on the provided set of parameters
-gen_modeldef_lin <- function( params )
+gen_modeldef_lin <- function()
 {
+    load( "data/lin.RData" )
     dd <- list()
-    dd[[1]] <- gelnet( params[[1]]$X ) + model_lin( params[[1]]$z ) +
-        rglz_L1( params[[1]]$l1 ) + rglz_L2( params[[1]]$l2 )
-    dd[[2]] <- dd[[1]] + model_lin( params[[2]]$z, params[[2]]$a ) +
-        rglz_L1( params[[2]]$l1, params[[2]]$d )
-    dd[[3]] <- dd[[2]] + rglz_L2( params[[3]]$l2, params[[3]]$P )
-    dd[[4]] <- dd[[3]] + rglz_L2( params[[4]]$l2, params[[4]]$P, params[[4]]$m )
-    dd[[5]] <- dd[[4]] + model_lin( params[[4]]$z, params[[4]]$a, fix_bias=TRUE )
-    dd[[6]] <- dd[[4]] + model_lin( params[[4]]$z, params[[4]]$a, nonneg=TRUE )
+    dd[[1]] <- gelnet( params$X ) + model_lin( params$z ) +
+        rglz_L1( params$l1 ) + rglz_L2( params$l2 )
+    dd[[2]] <- dd[[1]] + model_lin( params$z, params$a ) +
+        rglz_L1( params$l1, params$d )
+    dd[[3]] <- dd[[2]] + rglz_L2( params$l2, params$P )
+    dd[[4]] <- dd[[3]] + rglz_L2( params$l2, params$P, params$m )
+    dd[[5]] <- dd[[4]] + model_lin( params$z, params$a, fix_bias=TRUE )
+    dd[[6]] <- dd[[4]] + model_lin( params$z, params$a, nonneg=TRUE )
     dd
 }
 
 test_that( "Linear regression training", {
-    ## Silently trains a linear GELnet model using the provided parameters
-    ftrain <- gen_ftrain( gelnet_lin_opt )
+    ftrain <- partial_lin( gelnet_lin_opt )
+    fobj <- partial_lin( gelnet_lin_obj )
 
-    ## Generates a model evaluator using a given set of parameters
-    fgen <- function( prms )
-        { function( mdl ) { do.call( gelnet_lin_obj, c(mdl,prms) ) } }
-
-    ## Generate the models and matching objective functions
-    params <- gen_params_lin()
-    mm <- purrr::map( params, ftrain )
-    ff <- purrr::map( params, fgen )
+    mm <- purrr::map( ftrain, do.call, list(silent=TRUE) )
+    ff <- purrr::map( fobj, purrr::lift_dl )
 
     ## Verify the basic model
     expect_length( which( mm[[1]]$w != 0 ), 30 )
@@ -59,17 +46,18 @@ test_that( "Linear regression training", {
     expect_relopt( mm, ff )
 
     ## Test bias fixture
-    mm[[5]] <- ftrain( params[[4]], fix_bias=TRUE )
-    expect_equal( mm[[5]]$b, with(params[[4]], sum(a*z)/sum(a)) )
+    load( "data/lin.RData" )
+    mm[[5]] <- ftrain[[4]]( silent=TRUE, fix_bias=TRUE )
+    expect_equal( mm[[5]]$b, with(params, sum(a*z)/sum(a)) )
     expect_lt( ff[[4]](mm[[4]]), ff[[4]](mm[[5]]) )
 
     ## Test non-negativity
-    mm[[6]] <- ftrain( params[[4]], nonneg=TRUE )
+    mm[[6]] <- ftrain[[4]]( silent=TRUE, nonneg=TRUE )
     purrr::map( mm[[6]]$w, expect_gte, 0 )
     expect_lt( ff[[4]](mm[[4]]), ff[[4]](mm[[6]]) )
 
     ## Compose model definitions using the "grammar of modeling"
-    dd <- gen_modeldef_lin( params )
+    dd <- gen_modeldef_lin()
 
     ## Train based on model definitions
     ## Ensure equivalence to direct calling of gelnet_lin_opt()
@@ -77,10 +65,10 @@ test_that( "Linear regression training", {
     purrr::map2( mm, mdls, expect_equal )
 
     ## Test the L1 ceiling computation
-    l1c <- with( params[[4]], l1c_lin( X, z, l2, a, d, P, m ) )
-    m1 <- ftrain(params[[4]], l1=l1c)
-    m2 <- ftrain(params[[4]], l1=l1c-0.01)
-    expect_equal( sum(m1$w), 0 )
-    expect_length( which(m2$w != 0), 1 )
+    l1c <- with( params, l1c_lin( X, z, l2, a, d, P, m ) )
+    m1 <- gen_ftrain( gelnet_lin_opt )( params, l1=l1c-0.0001)
+    m2 <- gen_ftrain( gelnet_lin_opt )( params, l1=l1c+0.0001)
+    expect_length( which(m1$w != 0), 1 )
+    expect_equal( sum(m2$w), 0 )
 })
 
