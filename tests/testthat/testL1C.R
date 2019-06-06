4c2a674abf5ab2b684aba9720ebd455d4b29c1d5
ArtemSokolov <artem.sokolov@gmail.com>
2019-06-06 09:51:35 -0600

    Refactoring L1 ceiling tests

--- tests/testthat/testL1C.R
+++ tests/testthat/testL1C.R
@@ -1,59 +1,45 @@
 context( "L1 ceiling" )
 
-## Generates a silent model training function
-## prms - preset parameters
-## ... - dynamic parameters
-gen_ftrain <- function( f )
+source( "custom.R" )
+
+## Wrapper for testing L1 ceiling values
+## ftrain - model training function
+## params - set of parameters to supply
+## l1c - ceiling value to test
+test_l1c <- function( ftrain, params, l1c )
 {
-    function( prms, ... )
-    {
-        p <- purrr::list_modify( prms, silent=TRUE, ... )
-        do.call( f, p )
-    }
+    m1 <- partial2( ftrain, params, l1=l1c-0.0001 )(silent=TRUE)
+    m2 <- partial2( ftrain, params, l1=l1c+0.0001 )(silent=TRUE)
+    expect_length( which(m1$w != 0), 1 )
+    expect_equal( sum(m2$w), 0 )
 }
 
 test_that( "Linear regression", {
     load( "data/lin.RData" )
 
-    ## Test the L1 ceiling computation
+    ## L1 ceiling for linear regression
     l1c <- with( params, l1c_lin( X, z, l2, a, d, P, m ) )
-    m1 <- gen_ftrain( gelnet_lin_opt )( params, l1=l1c-0.0001)
-    m2 <- gen_ftrain( gelnet_lin_opt )( params, l1=l1c+0.0001)
-    expect_length( which(m1$w != 0), 1 )
-    expect_equal( sum(m2$w), 0 )
+    test_l1c( gelnet_lin_opt, params, l1c )
 })
 
 test_that( "Binary logistic regression", {
     load( "data/blr.RData" )
 
-    ## Set up the training function
-    ftrain <- gen_ftrain( gelnet_blr_opt )
+    ## L1 ceiling for binary logistic regression (unbalanced case)
+    l1c1 <- with( params, l1c_blr(X, y, l2, FALSE, d, P, m) )
+    ftrain1 <- purrr::partial( gelnet_blr_opt, eps=1e-20 )
+    test_l1c( ftrain1, params, l1c1 )
 
-    ## Test the L1 ceiling computation (unbalanced case)
-    l1c <- with( params, l1c_blr(X, y, l2, FALSE, d, P, m) )
-    m1 <- ftrain( params, l1=l1c-0.0001, eps=1e-20 )
-    m2 <- ftrain( params, l1=l1c+0.0001, eps=1e-20 )
-    expect_length( which(m1$w != 0), 1 )
-    expect_equal( sum(m2$w), 0 )
-
-    ## Test the L1 ceiling computation (balanced case)
-    l1cb <- with( params, l1c_blr(X, y, l2, TRUE, d, P, m) )
-    m1b <- ftrain( params, l1=l1cb-0.6, eps=1e-20 )
-    m2b <- ftrain( params, l1=l1cb+0.0001, eps=1e-20 )
-    expect_length( which(m1b$w != 0), 1 )
-    expect_equal( sum(m2b$w), 0 )
+    ## L1 ceiling for binary logistic regression (balanced case)
+    l1c2 <- with( params, l1c_blr(X, y, l2, TRUE, d, P, m) )
+    ftrain2 <- purrr::partial( gelnet_blr_opt, eps=1e-20, balanced=TRUE )
+    test_l1c( ftrain2, params, l1c2 )
 })
 
 test_that( "One-class logistic regression", {
     load( "data/oclr.RData" )
 
-    ## Set up the training function
-    ftrain <- gen_ftrain( gelnet_oclr_opt )
-
-    ## Test the L1 ceiling computation
+    ## L1 ceiling for one-class logistic regression
     l1c <- with( params, l1c_oclr(X, l2, d, P, m) )
-    m1 <- ftrain( params, l1=l1c )
-    m2 <- ftrain( params, l1=l1c-0.0001 )
-    expect_equal( sum(m1$w), 0 )
-    expect_length( which(m2$w != 0), 1 )
+    test_l1c( gelnet_oclr_opt, params, l1c )
 })
