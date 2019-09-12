1a8b742a55bc2fdd04b9a89ca3210faa76eccaa9
Artem Sokolov <artem.sokolov@gmail.com>
2019-09-12 16:51:50 -0400

    Tests for L1bin functionality

--- tests/testthat/testL1bin.R
+++ tests/testthat/testL1bin.R
@@ -21,13 +21,37 @@ test_that( "Linear regression", {
     mdf_nn <- with(params, gelnet(X) + model_lin(z,a,nonneg=TRUE) + rglz_L2(l2,P,m))
     test_l1bin( mdf_nn, params$d, c(1,5,10,23) )
 
-    ## Check for failure at 25 features
-    ## TODO
-    test_l1bin( mdf_nn, params$d, 25 )
+    ## Check for failure at 24 features
+    expect_error(test_l1bin( mdf_nn, params$d, 24 ), "Unable to reach")
 })
 
-## test_that( "Binary logistic regression", {
-##     load( "data/blr.RData" )
+test_that( "Binary logistic regression", {
+    load( "data/blr.RData" )
 
-    
-## })
+    mdf <- list()
+    yf <- factor(params$y, c(1,0))
+    mdf[[1]] <- with(params, gelnet(X) + model_blr(yf) + rglz_L2(l2,P,m))
+    mdf[[2]] <- with(params, gelnet(X) + model_blr(yf, balanced=TRUE) + rglz_L2(l2,P,m))
+    purrr::map( mdf, test_l1bin, params$d )
+
+    ## Maxes out at 25 non-negative features
+    mdf_nn <- with(params, gelnet(X) + model_blr(yf, nonneg=TRUE) + rglz_L2(l2,P,m))
+    test_l1bin( mdf_nn, params$d, c(1,5,10,25) )
+
+    ## Check for failure at 26 features
+    expect_error( test_l1bin(mdf_nn, params$d, 26), "Unable to reach" )
+})
+
+test_that( "One-class logistic regression", {
+    load( "data/oclr.RData" )
+
+    mdf <- with( params, gelnet(X) + rglz_L2(l2,P,m) )
+    test_l1bin( mdf, params$d )
+
+    ## Maxes out at 28 non-negative features
+    mdf_nn <- with(params, gelnet(X) + model_oclr(TRUE) + rglz_L2(l2,P,m))
+    test_l1bin( mdf_nn, params$d, c(1,5,10,28) )
+
+    ## Check for failure at 29 features
+    expect_error( test_l1bin(mdf_nn, params$d, 29), "Unable to reach" )
+})
